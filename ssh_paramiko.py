bf9501491a83a43e9ab329d23631165ab0866d66
prashantvaria <prashantvaria9@gmail.com>
2018-04-04 17:04:49 -0700

    added try and except for wrong password

--- ssh_paramiko.py
+++ ssh_paramiko.py
@@ -3,32 +3,37 @@ from netmiko import ConnectHandler  #importing netmiko
 import getpass
 import sys
 
-ip = sys.argv[1]
-user = sys.argv[2]
 
-if len(sys.argv)<2:
-	print "arguments missing\n"
-	print "Format = <python ssh.py> <Host ip address> <username>"
-	sys.exit(1)
+try:
 
-else:
-	#entering the parameters of router for login
-	cisco_router = {
-	     'host': ip,
-	     'device_type': 'cisco_ios',
-	     'username': user,
-	     'password': getpass.getpass(),
-	     'secret': 'cisco',
-	     }
+	if len(sys.argv)<2:
+		print "arguments missing\n"
+		print "Format = <python ssh.py> <Host ip address> <username>"
+		sys.exit(1)
 
-	output={}  # creating a empty dictionary for storing the output for future changes
-	connect = ConnectHandler(**cisco_router) #calling the parameter into the method and storing in variable
-	connect.enable() # this is to enter from usermode to privilege mode
-	output = connect.send_command ("show ip interface brief")
-	print ("{:>50}".format("This is Interface table\n"))
-	print (output)
+	else:
+		#entering the parameters of router for login
+		ip = sys.argv[1]
+		user = sys.argv[2]
+		cisco_router = {
+		     'host': ip,
+		     'device_type': 'cisco_ios',
+		     'username': user,
+		     'password': getpass.getpass(),
+		     'secret': 'cisco',
+		     }
 	
-	connect.disconnect()
+		output={}  # creating a empty dictionary for storing the output for future changes
+		connect = ConnectHandler(**cisco_router) #calling the parameter into the method and storing in variable
+		connect.enable() # this is to enter from usermode to privilege mode
+		output = connect.send_command ("show ip interface brief")
+		print ("{:>50}".format("This is Interface table\n"))
+		print (output)
+	
+		connect.disconnect()
+
+except:
+	print "The password you entered is not correct, Please start over"
 
 
 
